4af4e0d60d934efd4e42dc68b94a291dd2e28dfc
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2022-04-01 13:27:43 -0300

    Enhance: Finished home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,41 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { NextPage } from "next";
 
-import { Content, HighlightedText, Title, Wrapper } from "@/styles/Home.styles";
+import Link from "next/link";
+import {
+  Content,
+  Description,
+  HighlightedText,
+  LeftContent,
+  RightContent,
+  Title,
+  Wrapper,
+} from "@/styles/Home.styles";
 
 const Home: NextPage = () => {
   return (
     <Wrapper>
       <Content>
-        <div>
+        <LeftContent>
           <Title>
-            So, you want to travel to <HighlightedText>Space</HighlightedText>
+            So, you want to travel to
+            <br />
+            <br />
+            <HighlightedText>Space</HighlightedText>
           </Title>
-        </div>
+          <Description>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </Description>
+        </LeftContent>
+
+        <RightContent>
+          <Link href="/destination">
+            <a>Explore</a>
+          </Link>
+        </RightContent>
       </Content>
     </Wrapper>
   );
--- src/styles/Home.styles.ts
+++ src/styles/Home.styles.ts
@@ -6,16 +6,65 @@ export const Wrapper = styled.div`
   width: 100%;
   background: url(${BackgroundImage.src}) no-repeat;
   background-size: cover;
+
+  padding-top: 387px;
+  padding-inline: 165px;
 `;
 
 export const Content = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   place-items: center;
+
+  color: ${({ theme }) => theme.colors.light_text};
 `;
 
-export const Title = styled.h3``;
+export const LeftContent = styled.div`
+  max-width: 390px;
+`;
 
-export const HighlightedText = styled.span``;
+export const Title = styled.h5`
+  ${({ theme }) => theme.typography.Heading5}
 
-export const Description = styled.p``;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.light_purple};
+`;
+
+export const HighlightedText = styled.span`
+  ${({ theme }) => theme.typography.Heading1}
+
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.light_text};
+`;
+
+export const Description = styled.p`
+  ${({ theme }) => theme.typography.body}
+
+  margin-top: 24px;
+  line-height: 32px;
+
+  color: ${({ theme }) => theme.colors.light_purple};
+`;
+
+export const RightContent = styled.div`
+  height: 274px;
+  width: 274px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.light_text};
+
+  margin-left: auto;
+  margin-top: auto;
+
+  a {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    ${({ theme }) => theme.typography.Heading4}
+    color: ${({ theme }) => theme.colors.dark_text};
+    text-transform: uppercase;
+  }
+`;
